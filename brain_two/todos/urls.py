403157d211eb4960a26f6c44eba52571eabb4bb2
Matt Snyder <msnyd87@gmail.com>
2023-04-18 19:17:52 -0400

    Final Submission

--- brain_two/todos/urls.py
+++ brain_two/todos/urls.py
@@ -3,7 +3,7 @@ from todos.views import todo_list_list, todo_list_detail, todo_list_create
 
 
 urlpatterns = [
-    path('create/,', todo_list_create, name = 'todo_list_create'),
+    path('create/', todo_list_create, name = 'todo_list_create'),
     path('<int:id>/', todo_list_detail, name='todo_list_detail'),
     path('', todo_list_list, name='todo_list_list'),
 ]
