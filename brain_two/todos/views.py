a185ec091aea3c63f12780fadfccc7edad6bf144
Matt Snyder <msnyd87@gmail.com>
2023-04-18 14:34:51 -0400

    if this commit was applied, Feature 7 would be completed

--- brain_two/todos/views.py
+++ brain_two/todos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from todos.models import TodoList, TodoItem
 
 # Create your views here.
+def todo_list_list(request):
+    todo_lists = TodoList.objects.all()
+    context = {
+        'todo_lists': todo_lists
+    }
+    return render(request, 'todos/todo_list_list.html', context)
