890dad97a3cdf760ec0818963e92c710aa1b3eb7
Matt Snyder <msnyd87@gmail.com>
2023-04-18 17:43:26 -0400

    if this commit was applied, Feature 8 would be completed

--- brain_two/todos/urls.py
+++ brain_two/todos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from todos.views import todo_list_list
+from todos.views import todo_list_list, todo_list_detail
 
 
 urlpatterns = [
+    path('<int:id>/', todo_list_detail, name='todo_list_detail'),
     path('', todo_list_list, name='todo_list_list'),
 ]
--- brain_two/todos/views.py
+++ brain_two/todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from todos.models import TodoList, TodoItem
 
 # Create your views here.
@@ -8,3 +8,19 @@ def todo_list_list(request):
         'todo_lists': todo_lists
     }
     return render(request, 'todos/todo_list_list.html', context)
+
+def todo_list_detail(request, id):
+    todo_list = get_object_or_404(TodoList, id=id)
+    todo_items = TodoItem.objects.filter(list=todo_list)
+    context = {
+        "todo_list": todo_list,
+        "todo_items": todo_items
+    }
+    return render(request, "todos/todo_list_detail.html", context)
+
+def create_todo_list(request):
+    if request.method == "POST":
+        form = TodoListForm(request.POST)
+        if form.is_valid():
+            todo_list = form.save()
+            return redirect("todo_list_detail", id=todo_list.id)
